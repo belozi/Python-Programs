4b595af449de6873caad8776023ca1163e5113ee
belozi <robert.belozi.lee@gmail.com>
2014-11-30 16:43:58 -0500

    buggy

---
+++ Invent Games with Python/buggy.py
@@ -0,0 +1,14 @@
+# This is a program with a bug in it that I will fix using the debugger in IDLE.
+
+import random
+number1 = random.randint(1, 10)
+number2 = random.randint(1, 10)
+print('What is ' + str(number1) + ' + ' + str(number2) + '?')
+answer = input()
+if int(answer) == number1 + number2:
+      print('Correct!')
+else:
+      print('Nope! The answer is ' + str(number1 + number2))
+      
+          
+      
--- Invent Games with Python/dragon.py
+++ Invent Games with Python/dragon.py
@@ -13,6 +13,7 @@ def chooseCave():
     while cave != '1' and cave != '2':
         print('Which cave will you go into? (1 or 2)')
         cave = input()
+        
     return cave
 
 def checkCave(chosenCave):
